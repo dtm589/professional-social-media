e112987eaa675ea4337b920c101b9b433a76392e
BanhMiMan <iammrmoo@gmail.com>
2023-07-29 12:50:01 -0400

    new route under profileroutes w findbyPK

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const profileRoutes = require('./profileapi');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/profileapi', profileRoutes)
 
 module.exports = router;
